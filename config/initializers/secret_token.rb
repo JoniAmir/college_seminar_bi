dc6f8db45586af3213e53c8915521f1c5f668839
Joni <joni.amir@gmail.com>
2012-12-17 17:28:19 +0200

    added initially

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+College::Application.config.secret_token = 'dd0af435b03260f618193f63d4173029225a92c92598aacd0751db5a2e2ec9158d86fe7f9340114345eecf4d311a7bc7c47bdd9da7999df508b2da79ec8568ae'
