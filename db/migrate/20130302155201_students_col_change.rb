5a20971a43eb76b38b58d5ba4a7945ec60ad52d5
Lital Zubery <litalzubery@gmail.com>
2013-03-02 15:54:15 +0200

    migration split

---
+++ db/migrate/20130302155201_students_col_change.rb
@@ -0,0 +1,21 @@
+class StudentsColChange < ActiveRecord::Migration
+  def up
+  	add_column :students, :gender_code, :integer
+  	remove_column :students, :ba_minor
+  	remove_column :students, :ba_degree
+  	remove_column :students, :combined_grade
+  	remove_column :students, :institute
+  	add_column :students, :group_description_code, :integer 
+  	add_column :students, :school_code, :integer
+  end
+
+  def down
+  	remove_column :students, :gender_code
+  	add_column :students, :ba_minor, :string
+  	add_column :students, :ba_degree, :string
+  	add_column :students, :combined_grade, :float
+  	add_column :students, :institute, :string
+  	remove_column :students, :group_description_code
+  	remove_column :students, :school_code
+  end
+end
