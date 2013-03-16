1a7803c7efa6f676cdfbc92ad80ee0138524c021
Yoni Amir <joni.amir@gmail.com>
2013-03-16 19:57:36 +0200

    added styling for menu item of current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,13 @@ module ApplicationHelper
 	  #ActiveRecord::Base.colorize_logging = colorize
 	end
 
+
+	def nav_link(link_text, link_path)
+	  class_name = current_page?(link_path) ? 'current' : ''
+
+	  content_tag(:li, :class => class_name) do
+	    link_to link_text, link_path
+	  end
+	end
+
 end
