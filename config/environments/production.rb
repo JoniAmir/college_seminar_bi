855e276df2d76b4b1a49a8a8261159e64ff61e20
Yoni Amir <joni.amir@gmail.com>
2013-04-20 15:45:46 +0300

    another one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,9 @@ College::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
   
-  # config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
